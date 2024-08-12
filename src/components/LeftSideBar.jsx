6ea51797a3d18be2f43c8d64bfdf00f58db4633c
chowankumar <chowanmalhani5@gmail.com>
2024-08-12 21:12:53 +0500

    complete the ui of the LeftSideBar

---
+++ src/components/LeftSideBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import assets from "./../assets/assets"
+
+const LeftSideBar = () => {
+    return (
+        <div className='bg-[#001030]
+         text-white h-[75vh]'>
+
+            <div className="p-[20px]">
+                <div className="flex justify-between items-center">
+                    <img src={assets.logo} alt="" className='max-w-[140px]' />
+                    <div className='menu'>
+                        <img src={assets.menu_icon} alt=""  
+                        className='max-h-[20px] opacity-[0.6] cursor-pointer'/>
+                    </div>
+                </div>
+
+                <div 
+                className="bg-[#002670] 
+                flex items-center gap-[10px] 
+                p-2 mt-[20px]">
+                    <img src={assets.search_icon} alt="" width="16px" />
+                    <input type="text" placeholder='Search here..'
+                    className='bg-transparent border-none outline-none text-[11px]' />
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 h-[70%] overflow-y-scroll">
+                {
+                    Array(12).fill("").map((item,index)=>(
+                        <div key={index} className="flex items-center gap-3 p-y-[10px] pl-5 cursor-pointer text-[13px] hover:bg-[#077EFF]">
+                        <img src={assets.profile_img} alt="" className='w-[35px]
+                        aspect-[1/1] rounded-[50%]' />
+                        <div className='flex flex-col'>
+                            <p>Chowan Kumar</p>
+                            <span>Hello, How are you?</span>
+                        
+                        </div>
+                    </div>
+                    ))
+                }
+
+
+            </div>
+        </div>
+    )
+}
+
+export default LeftSideBar
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import LeftSideBar from '../components/LeftSideBar'
+import RightSideBar from '../components/RightSideBar'
+import ChatBox from "../components/ChatBox"
 
 const Chat = () => {
   return (
-    <div>
+
+    <div className='min-h-[100vh] bg-gradient-to-r from-[#596AFF] to-[#383699] flex items-center justify-center'>
+
+      <div className="w-[90%] h-[75vh]  max-w-[1300px] bg-white chat-container">
+
+        <LeftSideBar/>
+         <ChatBox/>
+         <RightSideBar/>
+      </div>
       
     </div>
   )
