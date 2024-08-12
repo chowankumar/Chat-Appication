2e986cb8d9946140e1ccefdbcfbbf415f855d440
chowankumar <chowanmalhani5@gmail.com>
2024-08-12 13:02:52 +0500

    create routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-     <div>
-      
-     </div>
-  )
-}
-
-export default App
+ import React from 'react'
+ import {Route,Routes} from 'react-router-dom'
+ import Login from "./pages/Login";
+ import Chat from "./pages/Chat"
+ import UpdateProfile from "./pages/ProfileUpdate"
+ 
+ const App = () => {
+   return (
+     <>
+     <Routes>
+      <Route path='/' element={<Login/>} />
+      <Route path='/chat' element={<Chat/>} />
+      <Route path='/profile' element={<UpdateProfile/>} />
+     </Routes>
+     </>
+   )
+ }
+ 
+ export default App
---
+++ src/pages/ProfileUpdate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileUpdate = () => {
+  return (
+    <div>profile page</div>
+  )
+}
+
+export default ProfileUpdate
