e593e5629111d355d754dd1d5b6053607b38bbec
chowankumar <chowanmalhani5@gmail.com>
2024-08-13 13:28:32 +0500

    integrate the firebase in the application

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyAtlTdls35Jxb3-IrzpUR62ZR9o-Kb4EIg",
+  authDomain: "chat-app-27532.firebaseapp.com",
+  projectId: "chat-app-27532",
+  storageBucket: "chat-app-27532.appspot.com",
+  messagingSenderId: "17379496746",
+  appId: "1:17379496746:web:ca332a1ca4f8d7b4ffc453"
+};
+
+const app = initializeApp(firebaseConfig);
+
