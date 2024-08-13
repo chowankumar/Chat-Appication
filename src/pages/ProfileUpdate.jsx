bef31b5f78d056664d1163ddcccf683f41a195ff
chowankumar <chowanmalhani5@gmail.com>
2024-08-13 12:14:19 +0500

    complet the ui of the all pages [add function in the update profule page when we upload the profile it will change dynamically and we select image from the device]

--- src/pages/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets from "./../assets/assets"
 
 const ProfileUpdate = () => {
+  const [image, setImage] = useState(false)
   return (
     <div style={{
       backgroundImage: `url(/background.png)`,
     }} className='profile  min-h-[100vh] flex items-center justify-center '>
-      <div className="profile-container bg-white flex items-center justify-between min-w-[700px] rounded-[10px]"> 
+      <div className="profile-container bg-white flex items-center justify-between min-w-[700px] rounded-[10px]">
         <form className='flex flex-col gap-[20px] p-[40px]' >
           <h3 className='font-[500]'>Pro file Details</h3>
           <label htmlFor="avatar" className='flex items-center gap-[10px] text-gray-500 cursor-pointer'>
 
-            <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
-            <img src={assets.avatar_icon} alt=""
-            className='w-[50px] rounded-[50%]' />
+            <input onClick={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={image?URL.createObjectURL(image):assets.avatar_icon} alt=""
+              className='w-[50px] rounded-[50%]' />
             upload profile image
           </label>
           <input className='p-[10px] min-w-[300px] border border-[#c9c9c9] outline-[#0773ff]' type="text" placeholder='your name' required />
@@ -23,7 +24,7 @@ const ProfileUpdate = () => {
 
         </form>
         <img className='profile-pic max-w-[160px]
-        my-[20px] mx-auto rounded-[50%]  ' src={assets.logo_icon} alt="" />
+        my-[20px] mx-auto rounded-[50%]  ' src={image?URL.createObjectURL(image):assets.logo_icon} alt="" />
       </div>
     </div>
   )
