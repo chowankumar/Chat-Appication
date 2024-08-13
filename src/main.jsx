c9a5a33e2b9e2a2dcdde8cd81cd1939a8075e691
chowankumar <chowanmalhani5@gmail.com>
2024-08-13 15:50:04 +0500

    set our Context in the main.js

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 
 
