d7e0231da834cb11b7604e6f9015ea15d82fb267
chowankumar <chowanmalhani5@gmail.com>
2024-08-13 15:53:33 +0500

    create state variable for the userdata and chat

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,8 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const  AppContext = createContext();
 
 const AppContextProvider = (props)=>{
+
+    const [userData,setUserData] = useState(null);
+    const[chatData,setChatData] = useState(null);
+
 
     const value ={
 
