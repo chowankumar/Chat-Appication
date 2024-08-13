9377a1a913c7aa6925b51cf65025c64ea64dbff8
chowankumar <chowanmalhani5@gmail.com>
2024-08-13 12:07:49 +0500

    complete the ui of the profileupdate

--- src/pages/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import assets from "./../assets/assets"
 
 const ProfileUpdate = () => {
   return (
-    <div>profile page</div>
+    <div style={{
+      backgroundImage: `url(/background.png)`,
+    }} className='profile  min-h-[100vh] flex items-center justify-center '>
+      <div className="profile-container bg-white flex items-center justify-between min-w-[700px] rounded-[10px]"> 
+        <form className='flex flex-col gap-[20px] p-[40px]' >
+          <h3 className='font-[500]'>Pro file Details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-[10px] text-gray-500 cursor-pointer'>
+
+            <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={assets.avatar_icon} alt=""
+            className='w-[50px] rounded-[50%]' />
+            upload profile image
+          </label>
+          <input className='p-[10px] min-w-[300px] border border-[#c9c9c9] outline-[#0773ff]' type="text" placeholder='your name' required />
+          <textarea className='p-[10px] min-w-[300px] border border-[#c9c9c9] outline-[#0773ff]' placeholder='Write profile bio' required name="" id=""></textarea>
+          <button type='submit' className='border-none text-white
+          bg-[#077eff] p-[8px] text-[16px] cursor-pointer' >Save</button>
+
+        </form>
+        <img className='profile-pic max-w-[160px]
+        my-[20px] mx-auto rounded-[50%]  ' src={assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
