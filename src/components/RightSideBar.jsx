3ca13f898cb2ba2a55724e5c2e9755c590061e94
chowankumar <chowanmalhani5@gmail.com>
2024-08-16 00:44:57 +0500

    show the pictures in meida section

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import assets from "./../assets/assets"
 import { logout } from '../config/firebase'
 import {AppContext} from "../context/AppContext"
@@ -6,28 +6,50 @@ import {AppContext} from "../context/AppContext"
 const RightSideBar = () => {
 
   const {chatUser,messages} = useContext(AppContext);
-  return (
+  const [msgImages,setMsgImages] = useState([]);
+
+  useEffect(()=>{
+    let tempVar = [];
+    messages.map((msg)=>{
+      if(msg.image){
+        tempVar.push(msg.image)
+      }
+    })
+    console.log(tempVar)
+    setMsgImages(tempVar)
+
+  },[messages])
+
+
+  return chatUser ?  (
     <div className='rs text-white bg-[#001030]
     relative h-[75vh] overflow-y-scroll '>
 
       <div className="rs-profile pt-[60px] text-center max-w-[70%] m-auto flex flex-col items-center">
-        <img src={assets.profile_img} alt=""
+        <img src={chatUser.userData.avatar} alt=""
         className='w-[110px] rounded-[50%]' />
         <h3 className='text-[18px] font-[400]
         flex items-center justify-center gap-[5px]
-        my-2'>Chowan Kumar <img src={assets.green_dot} alt="" /></h3>
-        <p className='text-[12px] opacity-[80%]font-[300]'>hey i am chowan kumar using this chatBox</p>
+        my-2'>{chatUser.userData.name} <img src={assets.green_dot} alt="" /></h3>
+        <p className='text-[12px] opacity-[80%]font-[300]'>{chatUser.userData.bio}</p>
       </div>
       <hr  className='border-[#ffffff50] my-[15px]'/>
       <div className="rs-media px-[20px] text-[15px]">
         <p>Media</p>
         <div className='max-h-[180px] overflow-y-scroll grid grid-cols-3 gap-[5px] mt-[8px]'>
-          <img src={assets.pic1} alt="" />
+          {
+            msgImages.map((url,index)=>(
+               <img src={url}
+               key={index} />
+
+            ))
+          }
+          {/* <img src={assets.pic1} alt="" />
           <img src={assets.pic2} alt="" />
           <img src={assets.pic3} alt="" />
           <img src={assets.pic4} alt="" />
           <img src={assets.pic1} alt="" />
-          <img src={assets.pic2} alt="" />
+          <img src={assets.pic2} alt="" /> */}
         </div>
       </div>
       <button className='absolute bottom-[10px] left-[25%] bg-[#077eff] text-white text-[12px] py-[10px] px-[65px]
@@ -36,6 +58,12 @@ const RightSideBar = () => {
 
     </div>
   )
+  :<div className='rs text-white bg-[#001030]
+    relative h-[75vh] overflow-y-scroll'>
+    <button className='absolute bottom-[40px] left-[25%] bg-[#077eff] text-white text-[12px] py-[10px] px-[65px]
+       rounded-[20px] cursor-pointer ' 
+       onClick={()=> logout(  )}>Logout</button>
+  </div>
 }
 
 export default RightSideBar
